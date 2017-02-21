94a275ed4e1649bf84eb8c2ee055245a95da1df2
Rebecca Kane <Rebecca Kane>
2017-02-21 15:27:45 +0000

    Basic assignment/subjects/timetable pages added

--- StuPla/src/pages/assignments/assignments.ts
+++ StuPla/src/pages/assignments/assignments.ts
@@ -3,17 +3,17 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
 /*
-  Generated class for the Timetable page.
+  Generated class for the Assignments page.
 
   See http://ionicframework.com/docs/v2/components/#navigation for more info on
   Ionic pages and navigation.
 */
 
 @Component({
-  selector: 'page-',
-  templateUrl: 'timetable.html'
+  selector: 'page-assignments',
+  templateUrl: 'assignments.html'
 })
-export class Timetable {
+export class Assignments {
 
   constructor(public navCtrl: NavController) {
     
