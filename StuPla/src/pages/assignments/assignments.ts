d12db64ee68e7123213f0b0e5145d382f635c409
Rebecca Kane <Rebecca Kane>
2017-02-25 21:39:35 +0000

    Updating and deleting from database

--- StuPla/src/pages/assignments/assignments.ts
+++ StuPla/src/pages/assignments/assignments.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 
-import { NavController, AlertController } from 'ionic-angular';
+import { NavController, AlertController, ActionSheetController } from 'ionic-angular';
 
 // Import AF2 List Observable for displaying contents of database
 import { AngularFire, FirebaseListObservable } from 'angularfire2';
@@ -21,45 +21,118 @@ export class Assignments {
 
     assignments: FirebaseListObservable<any>; // populate assignments var
 
-    constructor(public navCtrl: NavController, public alertCtrl: AlertController, af: AngularFire) {
+    constructor(public navCtrl: NavController, public alertCtrl: AlertController, public asCtrl: ActionSheetController, af: AngularFire) {
         // NavController allows navigation between pages, in this case the menu
 
         // Database reference, listens to "assignments" node in the Firebase database
         this.assignments = af.database.list('/assignments');
     } // end constructor
 
+
+// Add Assignment to database
     addAssignment(){
-        let prompt = this.alertCtrl.create({
-            title: 'New Assignment',
-            message: "New A",
-            inputs: [
+      let prompt = this.alertCtrl.create({
+        title: 'New Assignment',
+        inputs: [
+        {
+            name: 'title',
+            placeholder: 'Title'
+        },
+        ], // end inputs
+
+        buttons: [
             {
-                name: 'title',
-                placeholder: 'Title'
-            },
-            ], // end inputs
+            text: 'Cancel',
+            //handler: data => {
+            //console.log('Cancel clicked');
+            //}
+            }, // end Cancel button
+            {
+            text: 'Save',
+            handler: data => {
+                // pushes the data to the assignments database when user clicks "Add Assignment"
+            this.assignments.push({
+                title: data.title // Title in database = title in data, firebase creates object and assigns it an ID
+            });   
+            }
+            } // End save button
+        ] // End buttons
+      });
 
-            buttons: [
-              {
-                text: 'Cancel',
-                //handler: data => {
-                //console.log('Cancel clicked');
-                //}
-              }, // end Cancel button
-              {
-                text: 'Save',
-                handler: data => {
-                    // pushes the data to the assignments database when user clicks "Add Assignment"
-                this.assignments.push({
-                    title: data.title // Title in database = title in data, firebase creates object and assigns it an ID
-                });   
-                }
-              } // End save button
-            ] // End buttons
-        });
+      prompt.present(); // Display prompt
 
-    prompt.present(); // Display prompt
+    } // End addAssignment
 
+// Show options when assignment is clicked
+    showOptions(assignmentID, assignmentTitle) { // pass these to del/update functions
+      let actionSheet = this.asCtrl.create({
+        title: 'What do you want to do?',
+        buttons: [
+        {
+            text: 'Delete Assignment',
+            role: 'destructive',
+            handler: () => {
+            this.deleteAssignment(assignmentID);
+            }
+        },{
+            text: 'Edit Assignment',
+            handler: () => {
+            this.editAssignment(assignmentID, assignmentTitle);
+            }
+        },{
+            text: 'Cancel',
+            role: 'cancel',
+            handler: () => {
+            console.log('Cancel clicked');
+            }
+        }
+        ]
+      }); // end actionSheet var
+
+    actionSheet.present(); // Displays the action sheet
+
+    } // End showOptions
+  
+  // Delete Assignment
+    deleteAssignment(assignmentID){
+        this.assignments.remove(assignmentID);
+        // Searches database for assignment with correct ID and deletes
     }
+
+  // Edit Assignment stuff
+    editAssignment(assignmentID, assignmentTitle){
+      let prompt = this.alertCtrl.create({
+        title: 'Assignment Name',
+        message: "Update Assignment",
+        inputs: [
+          {
+            name: 'title',
+            placeholder: 'Title',
+            value: assignmentTitle // passes input to current assignmentTitle
+          },
+          ],
+
+        buttons: [
+          {
+            text: 'Cancel',
+            handler: data => {
+            console.log('Cancel clicked');
+            }
+          }, // End Cancel button
+          {
+            text: 'Save',
+            handler: data => {
+            // Passing updated assignment name to .update() function, which searches for ID and updates the name for it
+            this.assignments.update(assignmentID, {
+                title: data.title
+            });
+            }
+          } // End Save button
+        ] // End Buttons
+      });
+
+    prompt.present();
+
+    } // End editAssignment
 
 } // End Assignments class
